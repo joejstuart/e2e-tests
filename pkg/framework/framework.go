0156f4a0592f54a60a0e28f56a072cb959a0f911
Joe Stuart <joe.stuart@gmail.com>
2022-04-05 14:54:46 -0500

    fixing duplicate method

--- pkg/framework/framework.go
+++ pkg/framework/framework.go
@@ -93,7 +93,3 @@ func NewFramework() (*Framework, error) {
 		JvmbuildserviceController: jvmbuildserviceController,
 	}, nil
 }
-
-func ChainsSuiteDescribe(text string, body func()) bool {
-	return g.Describe("[chains-suite "+text+"]", g.Ordered, body)
-}
